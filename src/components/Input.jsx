1a12930a563399568530307e69f5c402149365ea
Tyler Hudson <tylerhudson96@gmail.com>
2023-11-13 13:12:05 -0500

    Drilling props down to allow input fields to update user information state

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,17 @@
-function Input({ formContent, onChange }) {
+import styles from "../styling/Input.module.css";
+
+function Input({ formContent, formData, onChange }) {
   return (
-    <div>
-      <label className="form-label">{formContent.label}</label>
-      <input type={formContent.type} onChange={onChange}></input>
-    </div>
+    <>
+      <label className={styles["form-label"]}>{formContent.label}</label>
+      <input
+        className={styles["form-input"]}
+        name={formContent.name}
+        type={formContent.type}
+        value={formData}
+        onChange={onChange}
+      ></input>
+    </>
   );
 }
 
