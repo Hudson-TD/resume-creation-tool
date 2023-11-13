796a27d9f791f16469c452d813b1519778ef5fca
Tyler Hudson <tylerhudson96@gmail.com>
2023-11-13 13:13:24 -0500

    Drilling props down to supply inputs with necessary functionality + added temp code to visualize and make sure state values are updating correctly

--- src/components/GeneralForm.jsx
+++ src/components/GeneralForm.jsx
@@ -1,14 +1,27 @@
 import Input from "./Input";
 
-function GeneralForm({ layout }) {
+function GeneralForm({ layout, formData, onChange }) {
   return (
     <div className="form-container">
       <h1>General Information</h1>
-      <form>
+      <form className="form-el">
         {layout.map((formEl) => {
-          return <Input formContent={formEl} key={formEl.id} />;
+          return (
+            <Input
+              formContent={formEl}
+              key={formEl.id}
+              onChange={onChange}
+              formData={formData[formEl.name]}
+            />
+          );
         })}
       </form>
+      {/* visual reference of state data on user input - remove later */}
+      <h1>Current State Data for General Form:</h1>
+      <p>{formData.firstName}</p>
+      <p>{formData.lastName}</p>
+      <p>{formData.emailAddress}</p>
+      <p>{formData.phoneNumber}</p>
     </div>
   );
 }
