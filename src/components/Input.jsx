03990893c592e0d032f61b4321299a26b079f6e7
Tyler Hudson <tylerhudson96@gmail.com>
2023-11-13 12:17:04 -0500

    Input refactored for reusability via parent form props

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,9 @@
 function Input({ formContent, onChange }) {
   return (
-    <>
+    <div>
       <label className="form-label">{formContent.label}</label>
       <input type={formContent.type} onChange={onChange}></input>
-    </>
+    </div>
   );
 }
 
