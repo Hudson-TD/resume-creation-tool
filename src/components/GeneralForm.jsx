3ab1fa24e58d17fc7db06668efd34970e391e193
Tyler Hudson <tylerhudson96@gmail.com>
2023-11-14 14:30:26 -0500

    Styling updates and text update for flow control button

--- src/components/GeneralForm.jsx
+++ src/components/GeneralForm.jsx
@@ -1,22 +1,50 @@
-import formStyling from "../styling/Form.module.css";
 import Input from "./Input";
 
-function GeneralForm({ layout, formData, onChange }) {
+function GeneralForm({ layout, formData, onChange, onSectionSave }) {
   return (
-    <div className={formStyling["form-container"]}>
-      <h1>General Information</h1>
-      <form className={formStyling["form-el"]}>
-        {layout.map((formEl) => {
-          return (
-            <Input
-              formContent={formEl}
-              key={formEl.id}
-              onChange={onChange}
-              formData={formData[formEl.name]}
-            />
-          );
-        })}
-      </form>
+    <div
+      className={
+        "w-100 d-flex flex-column justify-content-center align-items-center"
+      }
+    >
+      <h1 className={"py-3"}>
+        General Information{" "}
+        <span>
+          <button
+            type="button"
+            className="btn btn-light rounded-right"
+            data-toggle="tooltip"
+            data-placement="top"
+            title="All fields are required (*), unless marked as (optional)"
+          >
+            ?
+          </button>
+        </span>
+      </h1>
+      <div className={"w-50"}>
+        <form className={"form-control"}>
+          {layout.map((formEl) => {
+            return (
+              <Input
+                formContent={formEl}
+                key={formEl.id}
+                onChange={onChange}
+                formData={formData[formEl.name]}
+              />
+            );
+          })}
+          <div className="d-flex justify-content-center py-3">
+            <button
+              type="button"
+              data-next="Education"
+              onClick={onSectionSave}
+              className="btn btn-primary px-5"
+            >
+              Continue
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
