cee039e08e25be2d8b34de800a68e011037f0e9a
Tyler Hudson <tylerhudson96@gmail.com>
2023-11-14 14:27:41 -0500

    Styling and refactoring HTML design for stored data output

--- src/components/EducationCard.jsx
+++ src/components/EducationCard.jsx
@@ -1,13 +1,25 @@
-import cardStyles from "../styling/Card.module.css";
-
 function EducationCard({ data }) {
   return (
-    <div className={cardStyles["education-card"]}>
-      <p className={cardStyles["education-card-item"]}>{data.institute}</p>
-      <p className={cardStyles["education-card-item"]}>{data.educationLevel}</p>
-      <p className={cardStyles["education-card-item"]}>{data.major}</p>
-      <p className={cardStyles["education-card-item"]}>{data.startDate}</p>
-      <p className={cardStyles["education-card-item"]}>{data.graduationDate}</p>
+    <div className="card w-75 mb-1 d-flex flex-row">
+      <div className="card-body">
+        <h5 className="card-title">{data.institute}</h5>
+        <p className="card-text">
+          {data.educationLevel} of {data.major}
+        </p>
+        <p className="card-text">
+          {data.startDate} - {data.graduationDate}
+        </p>
+        <ul className="list-unstyled">
+          <li className="card-text">- {data.accoladeOne}</li>
+          <li className="card-text">- {data.accoladeTwo}</li>
+          <li className="card-text">- {data.accoladeThree}</li>
+        </ul>
+      </div>
+      <div>
+        <button href="#" className="btn btn-light h-100 w-15">
+          X
+        </button>
+      </div>
     </div>
   );
 }
