1bfe751fc57cdc95988bfa74c2683ba21b9e540b
Tyler Hudson <tylerhudson96@gmail.com>
2023-11-14 14:34:03 -0500

    Removal of obsolete styling in favor of Bootstrap integration

--- src/components/UserEducation.jsx
+++ src/components/UserEducation.jsx
@@ -1,15 +1,47 @@
 import EducationCard from "./EducationCard";
-import cardStyles from "../styling/Card.module.css";
 
-function UserEducation({ dataList }) {
+function UserEducation({ dataList, onSectionSave }) {
   return (
-    <>
-      <div className={cardStyles["education-cards-container"]}>
+    <div
+      className={
+        "w-100 d-flex flex-column justify-content-start align-items-center"
+      }
+    >
+      <h1 className={"py-3"}>
+        {dataList.length} / 3 Entries Used{" "}
+        <span>
+          <button
+            type="button"
+            className="btn btn-light rounded-right"
+            data-toggle="tooltip"
+            data-placement="top"
+            title="1 entry required, total entries limited to 3 for formatting purposes"
+          >
+            ?
+          </button>
+        </span>
+      </h1>
+      <div
+        className={
+          "w-100 d-flex flex-column justify-content-center align-items-center"
+        }
+      >
         {dataList.map((dataEntry, index) => {
           return <EducationCard key={index} data={dataEntry} />;
         })}
       </div>
-    </>
+      <div className="d-flex justify-content-center py-3">
+        <button
+          type="button"
+          data-next="Work Experience"
+          disabled={dataList.length >= 1 ? false : true}
+          onClick={onSectionSave}
+          className={"btn btn-primary px-5"}
+        >
+          Continue
+        </button>
+      </div>
+    </div>
   );
 }
 
