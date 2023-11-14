6ec72bcdcd80ba8b000680cea8bacb5a2318cb31
Tyler Hudson <tylerhudson96@gmail.com>
2023-11-14 14:28:57 -0500

    Updated styling and overall form size to share screen with UserEducation component

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -1,4 +1,3 @@
-import formStyling from "../styling/Form.module.css";
 import Input from "./Input";
 import Select from "./Select";
 
@@ -6,38 +5,46 @@ function EducationForm({ layout, formData, dataList, onChange, onSave }) {
   const maxEntries = 3;
 
   return (
-    <div className={formStyling["form-container"]}>
-      <h1>Education Information</h1>
-      <form className={formStyling["form-el"]}>
-        {layout.map((formEl) => {
-          if (formEl.fieldTag === "Input") {
-            return (
-              <Input
-                formContent={formEl}
-                key={formEl.id}
-                onChange={onChange}
-                formData={formData[layout.name]}
-              />
-            );
-          } else if (formEl.fieldTag === "Select") {
-            return (
-              <Select
-                formContent={formEl}
-                key={formEl.id}
-                formData={formData[layout.name]}
-                onChange={onChange}
-              />
-            );
-          }
-        })}
-
-        <button
-          disabled={dataList.length === maxEntries ? true : false}
-          onClick={onSave}
-        >
-          Save Entry
-        </button>
-      </form>
+    <div
+      className={
+        "w-100 d-flex flex-column justify-content-start align-items-center"
+      }
+    >
+      <h1 className={"py-3"}>Education Information</h1>
+      <div className={"w-75"}>
+        <form className={"form-group"}>
+          {layout.map((formEl) => {
+            if (formEl.fieldTag === "Input") {
+              return (
+                <Input
+                  formContent={formEl}
+                  key={formEl.id}
+                  onChange={onChange}
+                  formData={formData[layout.name]}
+                />
+              );
+            } else if (formEl.fieldTag === "Select") {
+              return (
+                <Select
+                  formContent={formEl}
+                  key={formEl.id}
+                  formData={formData[layout.name]}
+                  onChange={onChange}
+                />
+              );
+            }
+          })}
+          <div className="d-flex justify-content-center py-3">
+            <button
+              className="btn btn-success px-5"
+              disabled={dataList.length === maxEntries ? true : false}
+              onClick={onSave}
+            >
+              Save Entry
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
