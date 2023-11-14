6f32556650b07eda429ced4d8cacfa0ea159625c
Tyler Hudson <tylerhudson96@gmail.com>
2023-11-14 14:26:42 -0500

    Added conditional for tooltip render on graduationDate

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,18 +1,47 @@
-import formStyling from "../styling/Form.module.css";
-
 function Input({ formContent, formData, onChange }) {
-  return (
-    <>
-      <label className={formStyling["form-label"]}>{formContent.label}</label>
-      <input
-        className={formStyling["form-input"]}
-        name={formContent.name}
-        type={formContent.type}
-        value={formData}
-        onChange={onChange}
-      ></input>
-    </>
-  );
+  if (formContent.name === "graduationDate") {
+    return (
+      <div className="form-control">
+        <label className="form-label">
+          {formContent.label}
+          <span
+            style={{
+              width: "5px",
+              height: "10px",
+              paddingLeft: "20px",
+              fontWeight: "bold",
+              color: "#0d6efd",
+            }}
+            data-toggle="tooltip"
+            data-placement="top"
+            title="If you are currently working towards your degree please enter the expected graduation date."
+          >
+            ?
+          </span>
+        </label>
+        <input
+          className="form-control"
+          name={formContent.name}
+          type={formContent.type}
+          value={formData}
+          onChange={onChange}
+        ></input>
+      </div>
+    );
+  } else {
+    return (
+      <div className="form-control">
+        <label className="form-label">{formContent.label}</label>
+        <input
+          className="form-control"
+          name={formContent.name}
+          type={formContent.type}
+          value={formData}
+          onChange={onChange}
+        ></input>
+      </div>
+    );
+  }
 }
 
 export default Input;
